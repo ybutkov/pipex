d1154f7d7100e4e3ad0b2ea5db67678b2d046ebe
Yurii Butkov <ybutkov@2-E-1.42heilbronn.de>
2025-10-12 20:50:02 +0200

    fix tests

--- src/error/output_error.c
+++ src/error/output_error.c
@@ -6,7 +6,7 @@
 /*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 17:43:42 by ybutkov           #+#    #+#             */
-/*   Updated: 2025/10/12 20:32:34 by ybutkov          ###   ########.fr       */
+/*   Updated: 2025/10/12 20:49:14 by ybutkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,24 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+
+
+#ifdef __APPLE__
+
+void output_error(const char *obj, char *error)
+{
+	write(STDERR_FILENO, PIPEX, ft_strlen(PIPEX));
+	write(STDERR_FILENO, obj, ft_strlen(obj));
+	write(STDERR_FILENO, COLON, ft_strlen(COLON));
+	if (!error)
+		write(STDERR_FILENO, strerror(errno), ft_strlen(strerror(errno)));
+	else
+		write(STDERR_FILENO, error, ft_strlen(error));
+	write(STDERR_FILENO, "\n", 1);
+	// printf("%s%s%s\n", PIPEX, strerror(errno), message);
+}
+
+#elif defined(__linux__)
 
 void output_error(const char *obj, char *error)
 {
@@ -29,3 +47,5 @@ void output_error(const char *obj, char *error)
 	write(STDERR_FILENO, "\n", 1);
 	// printf("%s%s%s\n", PIPEX, strerror(errno), message);
 }
+
+#endif
